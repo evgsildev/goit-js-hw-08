5a6a817a6907f8bed31b749e5c408fbaaa24d606
Evgenii Silenko <evgen.silen@gmail.com>
2022-09-02 11:52:22 +0300

    HW8 fix 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,14 +4,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
-let currentTime;
+
+let currentTime = Number(localStorage.getItem(STORAGE_KEY));
 
 function getTime(e) {
   localStorage.setItem(STORAGE_KEY, e.seconds);
 }
 
 player.on('timeupdate', throttle(getTime, 1000));
-
-currentTime = localStorage.getItem(STORAGE_KEY);
 
 player.setCurrentTime(currentTime);
