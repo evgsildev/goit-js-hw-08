8562b63d13266114dc44235c6fe3469d63866d2e
Evgenii Silenko <evgen.silen@gmail.com>
2022-09-03 17:12:18 +0300

    task 3 fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,6 +3,7 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input');
 const message = document.querySelector('textarea');
+const button = document.querySelector('button');
 
 const STORAGE_KEY = 'feedback-form-state';
 
@@ -10,32 +11,43 @@ form.addEventListener('input', throttle(onFormInput, 500));
 
 form.addEventListener('submit', onFormSubmit);
 
-const dataObj = {};
+let dataObj = {};
 
 populateData();
-
-function onFormSubmit(evt) {
-  evt.preventDefault();
-
-  console.log(dataObj);
-
-  evt.currentTarget.reset();
-  localStorage.clear();
-}
 
 function onFormInput(evt) {
   dataObj[evt.target.name] = evt.target.value;
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataObj));
+
+  button.removeAttribute('disabled', 'disabled');
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+  localStorage.clear();
+
+  dataObj = {};
+  button.setAttribute('disabled', 'disabled');
 }
 
 function populateData() {
-  const saveMessage = localStorage.getItem(STORAGE_KEY);
+  const saveMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
   if (saveMessage) {
-    const values = JSON.parse(saveMessage);
+    dataObj = saveMessage;
 
-    email.value = values.email;
-    message.value = values.message;
+    if (dataObj.email) {
+      email.value = dataObj.email;
+    }
+
+    if (dataObj.message) {
+      message.value = dataObj.message;
+    }
   }
 }
